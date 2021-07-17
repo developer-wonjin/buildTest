65780e3023d79c9c0cb363e176bb64ea5bc403c3
wonjin <wonjin1776@naver.com>
2021-07-18 03:41:25 +0900

    Initial commit

---
+++ src/test/java/com/example/buildtest/BuildTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.buildtest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
